00201be894f7fcc4eccbfdfecd9b7a80c421073b
XoseCarlos <xosecarlosvazquez@gmail.com>
2021-11-16 17:27:49 +0100

    version 2.2 16/11/2021

--- app/src/main/java/com/josecarlos/maletavirtual/SplashScreenActivity.kt
+++ app/src/main/java/com/josecarlos/maletavirtual/SplashScreenActivity.kt
@@ -1,3 +1,16 @@
+/* *******************************************
+Alumno: José Carlos Vázquez Míguez
+Mail: xosecarlos@mundo-r.com
+Centro: ILERNA ONLINE
+Ciclo: DAM
+Curso: 2021-2022 (1º semestre)
+Proyecto: Maleta Virtual
+Tutor: Mario Gago
+Fecha última revisión: 15/11/2021
+Revisión: 1.0
+**********************************************
+*/
+
 package com.josecarlos.maletavirtual
 
 import android.content.Intent
