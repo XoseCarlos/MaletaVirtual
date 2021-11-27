53f035ce4f58517f72aeb93e2f86f2981bc2d9bd
XoseCarlos <xosecarlosvazquez@gmail.com>
2021-11-27 14:52:10 +0100

    version 4.5 Compartidas 27/11/2021

--- app/src/main/java/com/josecarlos/maletavirtual/utils/Utils.kt
+++ app/src/main/java/com/josecarlos/maletavirtual/utils/Utils.kt
@@ -126,7 +126,7 @@ class Utils : Application() {
         fun Fragment.esContrasenaValida(password: String): Boolean {
             // Necesita Contener -->    1 Num / 1 Minuscula / 1 Mayuscula / 1 Special / Min Caracteres 4
             // ya que incluye a los 4 casos. Puedo limitar el largo de la clave en el TextInputEditText
-            val passwordPattern = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*?[#?!@\$%^&*-])(?=\\S+$).{4,}$"
+            val passwordPattern = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*?[#?!@\$%^&_*-])(?=\\S+$).{4,}$"
             val pattern = Pattern.compile(passwordPattern)
             //return password.length>5
             return pattern.matcher(password).matches()
